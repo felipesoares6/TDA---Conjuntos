d36b25b53187896b3337db23f74f487c3d3adda3
Jean <jeanvitor_06@hotmail.com>
2016-05-19 15:54:48 -0300

    19/05

--- Conjuntos.c
+++ Conjuntos.c
@@ -23,14 +23,9 @@ void imprimir(Tdesc *);
 int pertinencia(Tdesc *, int);
 
 void uniao(Tdesc *,Tdesc *);
-
 void interseccao(Tdesc *,Tdesc *);
+void diferenca(Tdesc *,Tdesc *);
 
-void diferenca12(Tdesc *,Tdesc *);
-/*
-void diferenca21(Tlista *inicio,Tlista *inicio2);
-
-*/
 
 int main(){
     int vValor, Num, EscolheDif;
@@ -63,33 +58,48 @@ int main(){
     imprimir(ConjuntoB);
     printf("\n\n");
 
+    // while(1){
+    //    printf("Digite um numero para verificar sua pertinencia ou -999 para sair\n");
+    //    scanf("%d", &Num);
+    //    if(Num == -999)
+    //       break;
+    //    printf("Digite o conjunto A ou B\n");
+    //    scanf(" %c", &EscolheConj);
+    //    if(EscolheConj == 'A')
+    //      if(pertinencia(ConjuntoA, Num))
+    //        printf("O numero %d pertence ao conjunto A\n", Num);
+    //     else
+    //       printf("Valor não pertinente aos conjuntos\n");
+    //    if(EscolheConj == 'B')
+    //      if(pertinencia(ConjuntoB, Num))
+    //          printf("O numero %d pertence ao conjunto B\n", Num);
+    //    else
+    //       printf("Valor não pertinente aos conjuntos\n");
+    //
+    //
+    // }
+
+    printf("Digite 1 - Diferenca de A menos B ou Digite 2 - Diferenca de B menos A\n\n");
     while(1){
-       printf("Digite um numero para verificar sua pertinencia ou -999 para sair\n");
-       scanf("%d", &Num);
-       if(Num == -999)
+        scanf("%d", &EscolheDif);
+        if(EscolheDif == -999)
           break;
-       printf("Digite o conjunto A ou B\n");
-       scanf(" %c", &EscolheConj);
-       if(EscolheConj == 'A')
-         if(pertinencia(ConjuntoA, Num))
-           printf("O numero %d pertence ao conjunto A\n", Num);
-        else
-          printf("Valor não pertinente aos conjuntos\n");
-       if(EscolheConj == 'B')
-         if(pertinencia(ConjuntoB, Num))
-             printf("O numero %d pertence ao conjunto B\n", Num);
-       else
-          printf("Valor não pertinente aos conjuntos\n");
+        if(EscolheDif == 1)
+        {  printf("\nA diferença 1-2 eh: ");
+          diferenca(ConjuntoA, ConjuntoB);
+        }
+        if(EscolheDif == 2)
+        { printf("A diferença 2-1 eh: ");
+          diferenca(ConjuntoB, ConjuntoA);
+        }
     }
-
+    printf("\n\n");
+    printf("A uniao eh: ");
     uniao(ConjuntoA, ConjuntoB);
-
+    printf("\n");
+    printf("A interseccao eh: ");
     interseccao(ConjuntoA, ConjuntoB);
-
-    diferenca12(ConjuntoA, ConjuntoB);
-    /*
-    diferenca21(inicio, inicio2);
-    */
+    printf("\n");
     return 0;
 }
 
@@ -133,7 +143,6 @@ void uniao(Tdesc *descA,Tdesc *descB)
 {
   Tlista *aux, *aux2;
   int cont;
-  printf("A uniao eh: ");
   imprimir(descA);
   aux2 = descB->inicio;
   while(1)
@@ -150,7 +159,7 @@ void uniao(Tdesc *descA,Tdesc *descB)
     if(aux2->prox != NULL)
       aux2 = aux2->prox;
     else
-    { printf("\n\n");
+    {
       break;
     }
   }
@@ -160,7 +169,6 @@ void interseccao(Tdesc *descA, Tdesc *descB){
   Tlista *aux, *aux2;
   aux = descA->inicio;
   aux2 = descB->inicio;
-  printf("A interseccao eh: ");
   while(aux != NULL){
     while(aux2 != NULL){
         if(aux->valor == aux2->valor){
@@ -178,11 +186,10 @@ void interseccao(Tdesc *descA, Tdesc *descB){
   }
 }
 
-void diferenca12(Tdesc *descA,Tdesc *descB)
+void diferenca(Tdesc *descA,Tdesc *descB)
 {
   Tlista *aux, *aux2;
   int cont;
-  printf("A diferença 1-2 eh: ");
   aux = descA->inicio;
   while(1)
   { aux2 = descB->inicio;
@@ -198,36 +205,8 @@ void diferenca12(Tdesc *descA,Tdesc *descB)
     if(aux->prox != NULL)
       aux = aux->prox;
     else
-    { printf("\n\n");
-      break;
-    }
-  }
-}
-/*
-void diferenca21(Tlista *inicio,Tlista *inicio2)
-{
-  Tlista *aux, *aux2;
-  int cont;
-  printf("A diferença 2-1 eh: ");
-  aux2 = inicio2;
-  while(1)
-  { aux = inicio;
-    cont=0;
-    while(aux != NULL)
     {
-      if(aux->valor == aux2->valor)
-        cont++;
-      aux = aux->prox;
-    }
-    if(cont == 0)
-      printf("%d ",aux2->valor);
-    if(aux2->prox != NULL)
-      aux2 = aux2->prox;
-    else
-    { printf("\n\n");
       break;
     }
   }
 }
-
-*/
